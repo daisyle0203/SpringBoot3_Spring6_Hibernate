6521be6de6130c9f477273dd956b1bd33d118893
Daisy <daisy.le0203@gmail.com>
2023-06-05 22:35:18 -0500

    implementing lazy intialization

--- 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/common/BaseballCoach.java
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/common/BaseballCoach.java
@@ -3,12 +3,12 @@ package com.luv2code.springcoredemo.common;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CricketCoach implements Coach {
-    public CricketCoach() {
+public class BaseballCoach  implements  Coach{
+    public BaseballCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
     }
     @Override
     public String getDailyWorkout() {
-        return "Practice fast bowling for 15 minutes!!!";
+        return "Spend 30 minutes in batting practice";
     }
 }
--- 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
@@ -3,12 +3,13 @@ package com.luv2code.springcoredemo.common;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CricketCoach implements Coach {
-    public CricketCoach() {
+public class TennisCoach implements Coach{
+    public TennisCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
     }
     @Override
     public String getDailyWorkout() {
-        return "Practice fast bowling for 15 minutes!!!";
+        return null;
     }
+
 }
