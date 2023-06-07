ef1bd8e13b3629690365bf80c4f0e4b906f8c589
Daisy <daisy.le0203@gmail.com>
2023-06-06 23:54:02 -0500

    create new query to find all objects

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.luv2code.cruddemo.dao;
 
 import com.luv2code.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO {
@@ -29,6 +32,17 @@ public class StudentDAOImpl implements StudentDAO {
     @Override
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id); // id is the primary key
+    }
+
+    @Override
+    public List<Student> findAll() {
+        // create query
+        // Noted: Student is not database table name, it is a JPA entity class name
+        // All JPQL syntax is based on entity name and entity fields
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+
+        // return query result
+        return theQuery.getResultList();
     }
 
 }
